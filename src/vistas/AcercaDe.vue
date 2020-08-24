7c5db1fe802876c80eaad5f2ce4a737681c8d1c6
JesusJD-MF <JesusJD.MF@gmail.com>
2020-08-23 21:51:14 -0400

    actualización menor 2️

--- src/vistas/AcercaDe.vue
+++ src/vistas/AcercaDe.vue
@@ -2,9 +2,12 @@
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl">Cripto¢ambios v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
-      Proyecto para obtener las cotizaciones de las cryptomonedas más
-      importantes a traves de la API REST de Coincap.
-      <br />Este proyecto es utilizado en el curso de Vue.js Básico de Platzi
+      Aplicación que obtiene las cotizaciones de las cryptomonedas más
+      importantes, a través de la API REST de Coincap.
+      <br />
+      <small style="color: #cbd5e0">
+        Proyecto realizado a partir del curso de Vue.js Básico de Platzi
+      </small>
     </p>
     <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/">
       Volver a la pagina de Inicio
